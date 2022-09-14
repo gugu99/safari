4cb6a3b07677b8e6cc5cc664b3b813fc93f2a084
stringbuckwheat <stringbuckwheat@gmail.com>
2022-09-14 10:33:53 +0900

    [서경] test commit

--- safari/src/main/java/com/gd/safari/vo/ChatRoom.java
+++ safari/src/main/java/com/gd/safari/vo/ChatRoom.java
@@ -1,5 +1,12 @@
 package com.gd.safari.vo;
 
-public class ChatRoom {
+import lombok.Data;
 
+@Data
+public class ChatRoom {
+	private int chatRoomNo;
+	private String chatRoomName;
+	private String createDate;
+	private String updateDate;
+	private int workNo;
 }
