dc19ab0a67dcbe90d735b8ac94155ee8d3056ce2
Cho SeungHyun <iloveohau@naver.com>
2022-09-14 17:38:08 +0900

    [승현] 로그인 세션확인 필터 생성

---
+++ safari/src/main/java/com/gd/safari/filter/AuthFilter.java
@@ -0,0 +1,48 @@
+package com.gd.safari.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.boot.reactor.DebugAgentEnvironmentPostProcessor;
+
+import com.gd.safari.commons.TeamColor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@WebFilter("/safari/*")
+public class AuthFilter extends HttpFilter implements Filter {
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// 디버깅
+		log.debug(TeamColor.CSH + "***AuthFilter***");
+		
+		HttpSession session = null;
+		if(request instanceof HttpServletRequest) {
+			session = ((HttpServletRequest)request).getSession();
+			// 인증범위에 따라 분기시키기
+			if(session.getAttribute("login") == null) { // login에 값이 없을 경우 재요청하기
+				// 디버깅
+				log.debug(TeamColor.CSH + "AuthFilter login : null - 로그인 페이지로 이동");
+				((HttpServletResponse)response).sendRedirect(((HttpServletRequest)request).getContextPath() + "/login");
+				return;
+			}
+		} else {
+			log.debug(TeamColor.CSH + "AuthFilter login : 웹요청이 아닙니다.");
+			// 404 page를 띄어줄 수도 있고(redirect) 아니면  처리를 해야함
+			return;
+		}
+		
+		chain.doFilter(request, response);
+	}
+}
