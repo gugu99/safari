a5f183ef73fe48e9f12c50d73a70aa725618fcf4
Cho SeungHyun <iloveohau@naver.com>
2022-09-15 09:16:50 +0900

    [승현] AuthFilter 수정

--- safari/src/main/java/com/gd/safari/filter/AuthFilter.java
+++ safari/src/main/java/com/gd/safari/filter/AuthFilter.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.boot.reactor.DebugAgentEnvironmentPostProcessor;
 
 import com.gd.safari.commons.TeamColor;
+import com.gd.safari.vo.Member;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,11 +31,22 @@ public class AuthFilter extends HttpFilter implements Filter {
 		HttpSession session = null;
 		if(request instanceof HttpServletRequest) {
 			session = ((HttpServletRequest)request).getSession();
+			
+			// 세션에 값 분리하기 
+			Member member = (Member)session.getAttribute("login");
+			
 			// 인증범위에 따라 분기시키기
-			if(session.getAttribute("login") == null) { // login에 값이 없을 경우 재요청하기
+			if(member == null) { // login에 값이 없을 경우 재요청하기
 				// 디버깅
 				log.debug(TeamColor.CSH + "AuthFilter login : null - 로그인 페이지로 이동");
-				((HttpServletResponse)response).sendRedirect(((HttpServletRequest)request).getContextPath() + "/login");
+				((HttpServletResponse)response).sendRedirect(((HttpServletRequest)request).getContextPath() + "/account/login");
+				return;
+			}
+			
+			if("N".equals(member.getActive())) {
+				// 디버깅
+				log.debug(TeamColor.CSH + "AuthFilter login : 비활성화된 계정 - 계정잠금해제 페이지로 이동");
+				((HttpServletResponse)response).sendRedirect(((HttpServletRequest)request).getContextPath() + "/lock/unlock-user");
 				return;
 			}
 		} else {
