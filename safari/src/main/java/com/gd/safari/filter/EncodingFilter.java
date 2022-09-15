06e01acf02221c703b1fa2b56d9fb669910582eb
gugu99 <gde1002@naver.com>
2022-09-15 09:26:30 +0900

    [다은] 디버깅 코드 수정

--- safari/src/main/java/com/gd/safari/filter/EncodingFilter.java
+++ safari/src/main/java/com/gd/safari/filter/EncodingFilter.java
@@ -7,15 +7,24 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
 
+import com.gd.safari.commons.TeamColor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @WebFilter("/*")
-public class EncodingFilter implements Filter {
+public class EncodingFilter extends HttpFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		
 		request.setCharacterEncoding("utf-8");
-		System.out.println("***EncodingFilter***");
+		log.debug(TeamColor.GDE + "***EncodingFilter***");
+		
 		
 		chain.doFilter(request, response);
+		
 	}
+
 }
